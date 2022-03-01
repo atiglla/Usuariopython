4475dc5dde7a78467455ace833f2731eafc389d6
atiglla <99499241+atiglla@users.noreply.github.com>
2022-03-01 11:45:50 -0500

    segundo commit

--- User.py
+++ User.py
@@ -6,12 +6,12 @@ class User:
     def hacer_retiro (self, cantidad):
         self.soles-=cantidad
 
-    def haceer_deposito (self, cantidad):
+    def hacer_deposito (self, cantidad):
         self.soles+=cantidad
 
     def mostrar_balance_usuario (self):
         print(f"Usuario:{self.nombre}, Balance:{self.soles}")
 
     def trannsfer_dinero(self, otro_usuario, cantidad):
-        self.soles-cantidad
+        self.soles-=cantidad
         otro_usuario.soles+=cantidad
--- main.py
+++ main.py
@@ -2,13 +2,27 @@ from User import User
 
 alex=User("Alex")
 mary=User("Mary")
+jose=User("Jose")
 
-alex.haceer_deposito(1500)
+alex.hacer_deposito(1500)
+alex.hacer_deposito(1500)
+alex.hacer_deposito(1500)
 alex.hacer_retiro(1400)
-
 alex.mostrar_balance_usuario()
 
-alex.trannsfer_dinero(mary,50)
+mary.hacer_deposito(1000)
+mary.hacer_deposito(1000)
+mary.hacer_retiro(200)
+mary.hacer_retiro(200)
+mary.mostrar_balance_usuario()
 
-print(alex.soles)
-print(mary.soles)
+jose.hacer_deposito(2000)
+jose.hacer_retiro(100)
+jose.hacer_retiro(100)
+jose.hacer_retiro(100)
+jose.mostrar_balance_usuario()
+
+alex.trannsfer_dinero(jose,50)
+alex.mostrar_balance_usuario()
+jose.mostrar_balance_usuario()
+
