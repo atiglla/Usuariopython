d32a0cf58d6d8ae00d5ddb1870835ffacb892aba
atiglla <99499241+atiglla@users.noreply.github.com>
2022-03-01 11:58:44 -0500

    actualizando user

--- User.py
+++ User.py
@@ -5,13 +5,17 @@ class User:
 
     def hacer_retiro (self, cantidad):
         self.soles-=cantidad
+        return self
 
     def hacer_deposito (self, cantidad):
         self.soles+=cantidad
+        return self
 
     def mostrar_balance_usuario (self):
         print(f"Usuario:{self.nombre}, Balance:{self.soles}")
+        return self
 
     def trannsfer_dinero(self, otro_usuario, cantidad):
         self.soles-=cantidad
         otro_usuario.soles+=cantidad
+        return self
